bc7c0fd0db8588ad054e1a6db5477540b7951b15
Rijesh Rajan <rijesh.rajan@fishtownanalytics.com>
2024-01-03 11:42:26 +0000

    exposures

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -19,6 +19,7 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        customers.first_name||' '||customers.last_name as "customer name",
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
