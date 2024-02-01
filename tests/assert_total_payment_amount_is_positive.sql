4983a8eca0360f6f599b03812867013ef4371a89
Rijesh Rajan <rijesh.rajan@fishtownanalytics.com>
2024-02-01 21:22:26 +0000

    commit 5

--- tests/assert_total_payment_amount_is_positive.sql
+++ tests/assert_total_payment_amount_is_positive.sql
@@ -1,6 +1,12 @@
 -- Refunds have a negative amount, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail
 
+{{
+    config(
+       group='customer_success'
+    )
+}}
+
 select
     order_id,
     sum(amount) as total_amount
