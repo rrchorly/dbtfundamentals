4c4b5d8c8e6b9629d6e81658333412b598a47b4f
Rijesh Rajan <rijesh.rajan@fishtownanalytics.com>
2025-09-18 13:21:33 +0000

    commit 3 today

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,5 +1,3 @@
 {% macro cents_to_dollars(column_name,decimal_places =2 ) -%}
     round(1.0 * {{ column_name }} /100, {{ decimal_places }})
-
-    {% set pass_phrase = env_var('DBT_ENV_SECRET_VALUE') %}
 {% endmacro %}
